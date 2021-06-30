7379edbd9e8d50ce0f966044b6d78017bb1c1b95
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-06-30 14:08:21 -0300

    bug: testing 2 - [pending]

---
+++ src/pages/Explore/ExploreArea.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function ExploreArea() {
+  return (
+    <h1>Tela de explorar por local de origem/area</h1>
+  );
+}
---
+++ src/pages/Explore/ExploreDrinksFood.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function ExploreDrinksFood() {
+  return (
+    <h1>Tela de explorar bebidas ou comidas</h1>
+  );
+}
