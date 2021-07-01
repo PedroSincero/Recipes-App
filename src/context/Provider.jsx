1e9d0e15d74ef1af7759ef2d8c27c880c8aa0445
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-07-01 14:48:32 -0300

    Bug: Ajusting call api - [Pending]

--- src/context/Provider.jsx
+++ src/context/Provider.jsx
@@ -10,22 +10,26 @@ function Provider({ children }) {
   const handleButton = () => {
     let endpoint = '';
     const getAPI = async (endpoints) => {
-      const results = await fetch(endpoints).then((response) => response.json());
+      const results = await fetch(endpoints)
+        .then((response) => response
+          .json()).catch((error) => console.log(error));
       setData(results);
     };
-    if (radio === 'ingredient') {
-      endpoint = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${search}`;
-    }
-    if (radio === 'name') {
-      endpoint = `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`;
-    }
-    if (radio === 'firstLetter' && search.length <= 1) {
-      endpoint = `https://www.themealdb.com/api/json/v1/1/search.php?f=${search}`;
-    }
     if (radio === 'firstLetter' && search.length > 1) {
       alert('Sua busca deve conter somente 1 (um) caracter'); // eslint-disable-line no-alert
     }
-    getAPI(endpoint);
+    if (radio === 'ingredient') {
+      endpoint = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${search}`;
+      getAPI(endpoint);
+    }
+    if (radio === 'name') {
+      endpoint = `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`;
+      getAPI(endpoint);
+    }
+    if (radio === 'firstLetter' && search.length <= 1) {
+      endpoint = `https://www.themealdb.com/api/json/v1/1/search.php?f=${search}`;
+      getAPI(endpoint);
+    }
   };
 
   const contextValue = {
