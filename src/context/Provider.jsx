536630d408157e6246116b69d2961ba98ea44b7e
Maiara Borsatti <maiara.borsatti@gmail.com>
2021-07-05 14:41:25 -0300

    requisito 1 ao 8

--- src/context/Provider.jsx
+++ src/context/Provider.jsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import AppContext from './AppContext';
 
 function Provider({ children }) {
-  const contextValue = {
+  const [email, setEmail] = useState(false);
+  const [password, setPassword] = useState(false);
+  const [user, setUser] = useState([]);
 
+  const contextValue = {
+    email,
+    setEmail,
+    password,
+    setPassword,
+    user,
+    setUser,
   };
+
   return (
-    <AppContext.Provider value={ contextValue }>
-      {children}
-    </AppContext.Provider>
+    <AppContext.Provider value={ contextValue }>{children}</AppContext.Provider>
   );
 }
 
