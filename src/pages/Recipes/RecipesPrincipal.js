37fdc53aedcfa8b4f4ff68a3a7423475344d24f5
Maiara Borsatti <maiara.borsatti@gmail.com>
2021-07-05 16:38:23 -0300

    Fix: finalizado requisito 10

--- src/pages/Recipes/RecipesPrincipal.js
+++ src/pages/Recipes/RecipesPrincipal.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import HeaderWithButton from '../../components/HeaderWithButton';
 import Menu from '../../components/Menu';
 import Search from '../../components/Search';
 
 export default function RecipesPrincipal() {
+  const location = useLocation();
+  const nameTitle = () => {
+    if (location.pathname === '/bebidas') {
+      return (
+        <HeaderWithButton title="Bebidas" />
+      );
+    }
+    if (location.pathname === '/comidas') {
+      return (
+        <HeaderWithButton title="Comidas" />
+      );
+    }
+  };
+
   return (
     <>
-      <HeaderWithButton title="Comidas" />
+      {nameTitle()}
       <Search />
       <h1>Tela principal de receitas</h1>
       <Menu />
