1bbfe50cb1d01ba96fcc609d525f31f81ad60a87
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-07-05 18:15:15 -0300

    erro: putting information on the wrong branch - Req 33

--- src/pages/Recipes/RecipesDetails.js
+++ src/pages/Recipes/RecipesDetails.js
@@ -2,6 +2,23 @@ import React from 'react';
 
 export default function RecipesDetails() {
   return (
-    <h1>Tela de detalhes de uma receita</h1>
+    <div>
+      <h1>Tela de detalhes de uma receita</h1>
+      <div>
+        <h1 data-testid="recipe-title">{}</h1>
+        <img src="" alt="" data-testid="recipe-photo" />
+        <button type="button" data-testid="share-btn">compartilhar</button>
+        <button type="button" data-testid="favorite-btn">favoritar</button>
+        <button type="button" data-testid="recipe-category">categoria</button>
+        {/* {ingredientes.map((info, index) => (
+          <li key={ index } data-testid={ `${index}-ingredient-name-and-measure` }>{info}</li>
+        ))} */}
+        <p data-testid="instructions"> Instruções</p>
+        {/* {recomendados.map((info, index) => (
+          <img key={ index } src="" alt="" data-testid={ `${index}-recomendation-card` } />
+        ))} */}
+        <button type="button" data-testid="start-recipe-btn">Iniciar Receita</button>
+      </div>
+    </div>
   );
 }
