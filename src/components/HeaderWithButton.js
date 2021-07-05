1a1e97bf2e0a4c39a5e667a00fc241477cec7689
Maiara Borsatti <maiara.borsatti@gmail.com>
2021-07-05 18:29:35 -0300

    Feat: requisito 12

--- src/components/HeaderWithButton.js
+++ src/components/HeaderWithButton.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useHistory } from 'react-router-dom';
+import Search from './Search';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
 export default function HeaderWithButton({ title }) {
   const history = useHistory();
   return (
-    <header>
-      <button type="button" onClick={ () => history.push('/perfil') }>
-        <img data-testid="profile-top-btn" src={ profileIcon } alt="Profile" />
-      </button>
-      <h3 data-testid="page-title">{title}</h3>
-      <button type="button">
-        <img data-testid="search-top-btn" src={ searchIcon } alt="Search" />
-      </button>
-    </header>
+    <>
+      <header>
+        <button type="button" onClick={ () => history.push('/perfil') }>
+          <img data-testid="profile-top-btn" src={ profileIcon } alt="Profile" />
+        </button>
+        <h3 data-testid="page-title">{title}</h3>
+        <button type="button">
+          <img data-testid="search-top-btn" src={ searchIcon } alt="Search" />
+        </button>
+      </header>
+      {/* <Search /> */}
+    </>
   );
 }
 
--- src/pages/Recipes/RecipesPrincipal.js
+++ src/pages/Recipes/RecipesPrincipal.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { useLocation } from 'react-router-dom';
 import HeaderWithButton from '../../components/HeaderWithButton';
 import Menu from '../../components/Menu';
-import Search from '../../components/Search';
 
 export default function RecipesPrincipal() {
   const location = useLocation();
@@ -22,7 +21,6 @@ export default function RecipesPrincipal() {
   return (
     <>
       {nameTitle()}
-      <Search />
       <h1>Tela principal de receitas</h1>
       <Menu />
     </>
