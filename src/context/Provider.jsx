ca0211b9ec08326aa6db51cf090276db29b6ad95
Luiz Reina <luizreeina@gmail.com>
2021-07-07 19:27:31 -0300

    Feat: requirements 60, 61, 62, 63, 65, 66 [DONE]

--- src/context/Provider.jsx
+++ src/context/Provider.jsx
@@ -17,6 +17,7 @@ function Provider({ children }) {
   const [category, setCategories] = useState();
   const [categoryDrink, setCategoriesDrink] = useState();
   const [doneRecipesList, setDoneRecipesList] = useState([]);
+  const [favoriteRecipesList, setFavoriteRecipesList] = useState([]);
   useEffect(() => {
     const getFood = async (endpoints) => {
       const limit = 12;
@@ -135,6 +136,8 @@ function Provider({ children }) {
     categoryDrink,
     doneRecipesList,
     setDoneRecipesList,
+    favoriteRecipesList,
+    setFavoriteRecipesList,
   };
 
   return (
