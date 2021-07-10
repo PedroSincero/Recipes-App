5ece4bd7884f4ca62ea9991e61dda21b9f3f5aaa
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-07-10 16:28:35 -0300

    saving

--- src/pages/Recipes/RecipesProgress.js
+++ src/pages/Recipes/RecipesProgress.js
@@ -1,7 +1,117 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import CopyToClipboard from 'react-copy-to-clipboard';
+import AppContext from '../../context/AppContext';
+import shareIcon from '../../images/shareIcon.svg';
 
-export default function RecipesProgress() {
+export default function RecipesProgress({ match: { params: { id } } }) {
+  const location = useLocation();
+  const pathnameBebidas = location.pathname === `/bebidas/${id}/in-progress`;
+  const {
+    detailsRecipe,
+    setDetailsRecipe, setFoodEndPoint, setDrinkEndpoint } = useContext(AppContext);
+  const [copied, setCopied] = useState(false);
+  const FOUR_SECONDS = 4000;
+
+  useEffect(() => {
+    if (location.pathname === `/bebidas/${id}/in-progress`) {
+      const getDetails = async (detailID) => {
+        const { drinks } = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${detailID}`).then((r) => r.json());
+        setDetailsRecipe(drinks);
+      };
+      getDetails(id);
+      setFoodEndPoint('https://www.themealdb.com/api/json/v1/1/search.php?s=');
+    }
+    if (location.pathname === `/comidas/${id}/in-progress`) {
+      const getDetails = async (detailID) => {
+        const { meals } = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${detailID}`).then((r) => r.json());
+        setDetailsRecipe(meals);
+      };
+      getDetails(id);
+      setDrinkEndpoint('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
+    }
+  }, [id, location, setDetailsRecipe, setFoodEndPoint, setDrinkEndpoint]);
+
+  const drinkDetails = () => {
+    const NUMBER_THREE = 3;
+    const NUMBER_EIGHT = 8;
+    const limit = pathnameBebidas ? NUMBER_THREE : NUMBER_EIGHT;
+    const result = [];
+    for (let index = 1; index <= limit; index += 1) {
+      const ingredient = detailsRecipe[0][`strIngredient${index}`];
+      const measure = detailsRecipe[0][`strMeasure${index}`];
+      if (ingredient !== null) {
+        const func = () => (measure === null ? result.push(ingredient)
+          : result.push(`${ingredient} - ${measure}`));
+        func();
+      }
+    }
+    const { strDrink,
+      strMeal,
+      strDrinkThumb, strCategory, strMealThumb, strInstructions } = detailsRecipe[0];
+
+    return (
+      <>
+        <h1 data-testid="recipe-title">{pathnameBebidas ? strDrink : strMeal}</h1>
+        <p data-testid="recipe-category">
+          {pathnameBebidas ? `${strCategory} Alcoholic` : strCategory}
+        </p>
+        <img
+          src={ pathnameBebidas ? strDrinkThumb : strMealThumb }
+          alt="Thumbnail"
+          data-testid="recipe-photo"
+        />
+        <p data-testid="instructions">{strInstructions}</p>
+        <iframe src="https://youtu.be/7atZfX85nd4" data-testid="video" title=" video teste" />
+        {result.map((info, index) => (
+          <li
+            key={ index }
+            data-testid="ingredient-step"
+          >
+            {info}
+          </li>
+        ))}
+      </>
+    );
+  };
+  console.log('detailsRecipe', detailsRecipe);
   return (
-    <h1>Tela de receita em progresso</h1>
+    <>
+      <h1>Tela de receita em progresso</h1>
+      {detailsRecipe && drinkDetails()}
+      <CopyToClipboard
+        text={ `http://localhost:3000${location.pathname}` }
+        onCopy={ () => {
+          setCopied(true);
+          setTimeout(() => setCopied(''), FOUR_SECONDS);
+        } }
+      >
+        <button
+          data-testid="share-btn"
+          type="button"
+          src={ shareIcon }
+        >
+          <input
+            type="image"
+            src={ shareIcon }
+            alt="share"
+          />
+        </button>
+      </CopyToClipboard>
+      {
+        copied && <span style={ { color: 'red' } }>Link copiado!</span>
+      }
+      <button type="button" data-testid="favorite-btn">favoritar</button>
+      <button type="button" data-testid="finish-recipe-btn">Finalizar receita</button>
+    </>
   );
 }
+
+RecipesProgress.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }).isRequired,
+};
